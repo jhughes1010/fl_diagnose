6d1497fbb350de003069c134ced2524c4e82ff57
James Hughes <James.Hughes@Microchip.com>
2022-05-13 08:27:23 -0600

    added single lines for opening file and creating JSON

--- import.rb
+++ import.rb
@@ -1,11 +1,17 @@
 #James Hughes 2022-05-13
 
-#Parser for taking raw flight infor to JSON for pilot evaluation
+#Parser for taking raw flight information to JSON for pilot evaluation
 require 'date'
 require 'json'
 
 #Read raw txt file
+#raw is an array of strings
+File.open("ual.txt" do |log_file|
+  raw = log_file.readlines
+end
+  
 
 #Parse txt file to array of hashes
 
 #Output array to JSON
+File.write('./flights.json', JSON.dump(flights))
