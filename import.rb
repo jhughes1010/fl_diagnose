54f630b1be1a33ded0021d686e7c1850f2930034
James Hughes <James.Hughes@Microchip.com>
2022-05-13 08:03:10 -0600

    added import.rb to project

---
+++ import.rb
@@ -0,0 +1,11 @@
+#James Hughes 2022-05-13
+
+#Parser for taking raw flight infor to JSON for pilot evaluation
+require 'date'
+require 'json'
+
+#Read raw txt file
+
+#Parse txt file to array of hashes
+
+#Output array to JSON
