5c50f66dfb716785094d0b58ebabe6adad89e11f
James Hughes <jhughes1010@gmail.com>
2022-06-10 05:30:23 -0600

    multiple lines parsed

--- import.rb
+++ import.rb
@@ -14,23 +14,33 @@ File.open("ual.txt") do |log_file|
   raw = log_file.readlines
 end
 
+
+
+flight = Hash.new
 #Parse txt file to array of hashes
 raw.each do |l|
-  if l.include?('TAFB')
-    flight = Hash.new
-    i_pos = l.split.index('TAFB-')
-    flight[:tafb] = l.split[i_pos+1]
-    flights << flight
+  if l.include?('BASIC')
+    i_pos = l.split.index('ID')
+    flight[:id] = l.split[i_pos+1]
     #puts '-----'
-    #puts flight[:tafb]
-    #puts flight
+    #puts flight[:id]
+  end
+  
+  if l.include?('TAFB')
+    l.strip!.gsub!(/[*]|[-]/,' ')
+    #puts l
+    
+    i_pos = l.split.index('CRD')
+    flight[:crd] = l.split[i_pos+1]
+    
+    i_pos = l.split.index('TAFB')
+    flight[:tafb] = l.split[i_pos+1]  
+    
+    flights << flight
+    flight = Hash.new
   end
 end
-
-
-  
-
-
+puts flights.size.to_s + ' records parsed'
 
 #Output array to JSON
 File.write('./flights.json', JSON.dump(flights))
